fa2ff72224715b0f14834fd3a2915ffea7f052b2
ZiTian.Zhao <skyloevil@gmail.com>
2025-06-30 16:19:40 +0000

    Refactor optimizer configuration in GPT class to handle weight decay and learning rate, and implement fused AdamW support.
    Signed-off-by: ZiTian.Zhao <skyloevil@gmail.com>

--- pytorch_gpt2_from_scratch/train_gpt.py
+++ pytorch_gpt2_from_scratch/train_gpt.py
@@ -224,21 +224,29 @@ class GPT(nn.Module):
 
     def configure_optimizers(self,weight_decay,learning_rate,device):
         param_dict = {pn: p for pn,p in self.named_parameters()}
+        # filter out parameters that do not require gradients
         param_dict = {pn: p for pn,p in param_dict.items() if p.requires_grad}
 
+        # separate parameters into those that require weight decay and those that do not
         decay_params = [p for n,p in param_dict.items() if p.dim() >= 2 ]
+        # typically, weight decay is applied to parameters that are matrices or tensors (e.g., weights)
         nodecay_params = [p for n,p in param_dict.items() if p.dim() < 2 ]
         optim_groups = [
             {'params': decay_params, 'weight_decay': weight_decay},
             {'params': nodecay_params, 'weight_decay': 0.0}
         ]
+        # print some statistics about the parameters
         num_decay_params = sum(p.numel() for p in decay_params)
+        # this is the total number of parameters that will be decayed
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
         print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # fuse available in torch 1.13+ , inspect the signature of AdamW to check if fused is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        # check if we are using a CUDA device
         use_fused = fused_available and device_type == "cuda"
         print(f"using fused AdamW: {use_fused}")
+        # create the optimizer, using AdamW with the specified learning rate and betas
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
 
@@ -417,7 +425,8 @@ def get_lr(it):
 #--------------------------------------------------------------------------------------
 
 #optimize!
-optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95),eps=1e-8)
+#optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95),eps=1e-8)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
 for i in range(50):
     t0 = time.time()
     x,y = train_loader.next_batch()
